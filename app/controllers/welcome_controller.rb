3e18ca35112050a0e03fb4aece8255eef810398f
Shrotri <U0126252@ten.thomsonreuters.com>
2017-02-16 18:39:06 +0530

    init

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-	@app_url = ENV['URL']
+	@app_url = ENV['APP_URL']
+	logger.debug("This should appear in log")
+	logger.debug("APP URL shoudl be printed here: '#{@app_url}'")
   end
 end
